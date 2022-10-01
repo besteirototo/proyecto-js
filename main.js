b1d039f2099075134e2fe748a76a0ac16a5313bb
Tobias Besteiro <besteirotobias@outlook.com>
2022-10-01 12:09:52 -0300

    Se agregó un constructor y un array de productos.

--- main.js
+++ main.js
@@ -2,67 +2,73 @@ const tienda = document.getElementById('tienda');
 const totalProductos = document.getElementById('totalProductos');
 const carro = document.getElementById('carro');
 
-//importa los productos del json local
+//importa los productos del json
 fetch('./JSON/productos.json')
 .then(response => response.json())
-.then(data => {
-    const productos = data;
-    localStorage.setItem("productos", JSON.stringify(productos));
-    //crea las tarjetas de los productos
-    productos.forEach(producto => {
-    const { id, nombre, precio, imagen } = producto;
-    tienda.innerHTML += `
-        <div class="producto">
-            <div class="container">
-                <img class="producto_imagen" src="${imagen}" alt="">
-                <div class="container">
-                    <h2 class="fs-6">${nombre}</h2>
-                    <p class="fs-4 fw-bold">$${precio}</p>
-                    <button id="${id}" class="comprar btn btn-outline-success">Comprar</button>
-                </div>
-            </div>
-        </div>
-    `;
-})})
+.then(data => localStorage.setItem("productos", JSON.stringify(data)))
 .catch(error => console.log(error))
 
-//función que escucha los clicks en la sección tienda
-tienda.addEventListener('click', e => {
-    comprar(e);
-});
-
-const comprar = (e) => {
-    //si contiene la clase comprar, genera una notificación
-    if(e.target.classList.contains('comprar')) {
-        Toastify({
-            text: "Éste producto fue agregado al carrito",
-            duration: 2000,
-            position: "center",
-            newWindow: true,
-            gravity: "top",
-            stopOnFocus: true,
-            style: {
-                background: "#2FBF71",
-            }
-        }).showToast(); 
-        //obtiene los productos del local storage y aumenta la cantidad si el id coincide
-        const productos = JSON.parse(localStorage.getItem("productos"));
-        const id = e.target.id;
-        productos.forEach(producto => {
-            id == producto.id ? producto.cantidad++ : null;
-            if (producto.cantidad > 0) {
-                compra(producto);
-            }
-            localStorage.setItem("productos", JSON.stringify(productos));
-        })
-        //aumenta el número de productos en el carrito
-        totalProductos.textContent++;
+//constructor de los productos
+class Producto {
+    constructor (id, nombre, precio, imagen, cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.cantidad = cantidad;
     }
-}   
+}
 
+//array de productos
+const productos = [];
+
+//genera nuevos objetos producto y los agrega al array
+const guardarProductos = () => {
+    JSON.parse(localStorage.getItem("productos")).forEach(producto => {
+        productos.push(new Producto(producto.id, producto.nombre, producto.precio, producto.imagen, producto.cantidad));
+    })
+}
+guardarProductos();
+
+//genera las cards de los productos con los datos enviados al array
+const generarProductos = () => {
+    productos.forEach(producto => {
+        const { id, nombre, precio, imagen } = producto;
+        tienda.innerHTML += `
+            <div class="card m-3">
+                <img src="${imagen}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${nombre}</h5>
+                    <p class="card-text">$${precio}</p>
+                    <button id=${id} class="comprar btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        `;
+    })
+}
+generarProductos();
+
+//agrega productos al carrito, emite una notificación, actualiza el total de productos y guarda la compra en el localStorage
+tienda.addEventListener('click', e => {
+    e.target.classList.contains('comprar') ? productos.find(producto => { producto.id == e.target.id ? (producto.cantidad += 1, localStorage.setItem(`compra${producto.id}`, JSON.stringify(producto)), totalProductos.textContent++, compra(producto), Toastify({ text: "El producto fue agregado al carrito", duration: 2000, position: "center", style: { background: "#06d6a0"} }).showToast()) : null }) : null;
+    if (productos.find(producto => producto.id == e.target.id)) {
+        const total = productos.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
+        document.getElementById('totalPrecio').textContent = `Total: $${total}`;
+    }
+})
+
+//elimina el producto del carrito, emite una notificación y actualiza el total de productos
+carro.addEventListener('click', e => {  
+    e.target.classList.contains('eliminar') ? productos.find(producto => { producto.id == e.target.id ? (producto.cantidad > 0 ? producto.cantidad -= 1 : e.target.parentElement.parentElement.remove(), producto.cantidad == 0 ? e.target.parentElement.parentElement.remove() : null, localStorage.setItem(`compra${producto.id}`, JSON.stringify(producto)), totalProductos.textContent--, compra(producto), Toastify({ text: "El producto fue eliminado del carrito", duration: 2000, position: "center", style: { background: "#0d1321"} }).showToast()) : null }) : null;
+    if (productos.find(producto => producto.id == e.target.id)) {
+        const total = productos.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
+        document.getElementById('totalPrecio').textContent = `Total: $${total}`;
+    }
+})
+
+//imprime los productos seleccionados en el carrito (valida que no haya productos repetidos)
 const compra = (producto) => {
-    //si la cantidad del producto es 1 y no existe el elemento con la clase eliminar(id), crea la tarjeta del producto en el carrito
-    if (producto.cantidad === 1 && document.querySelector(`.eliminar${producto.id}`) === null) {
+    if (producto.cantidad === 1 && document.getElementsByClassName(`eliminar${producto.id}`).length === 0) {    
         const { id, nombre, precio, imagen, cantidad } = producto;
         carro.innerHTML += `
                 <div class="carro_producto">
@@ -70,48 +76,15 @@ const compra = (producto) => {
                     <div class="carro_info">
                         <h3 class="fs-6 m-0">${nombre}</h3>
                         <p class="m-0">$${precio}</p>
-                        <p class="m-0">Cantidad: ${cantidad}</p>
-                        <button id=${id} class="eliminar${id} btn btn-danger btn-sm">Eliminar</button>
+                        <p class="m-0">${cantidad}</p>
+                        <button id=${id} class="eliminar eliminar${id} btn btn-danger btn-sm">Eliminar</button>
                     </div>
                 </div>
             `;
     } else {
         //aumenta la cantidad del producto
-        const { id, cantidad } = producto;
-        const carroProducto = document.getElementById(`${id}`);
-        carroProducto.parentElement.children[2].textContent = `Cantidad: ${cantidad}`;
+        document.getElementById(`${producto.id}`).parentElement.children[2].textContent = `Cantidad: ${producto.cantidad}`;
     }
-    eliminarProducto(producto);
-}
-
-//función que elimina los productos del carrito
-const eliminarProducto = (producto) => {
-    carro.addEventListener('click', e => {  
-        if (e.target.classList.contains(`eliminar${producto.id}`)) {
-            const { id } = producto;
-            const eliminar = document.querySelector(`.eliminar${id}`);
-            const productos = JSON.parse(localStorage.getItem("productos"));
-            productos.forEach(producto => {
-                if (producto.id == id) {
-                    producto.cantidad = 0;
-                    totalProductos.textContent > 0 ? totalProductos.textContent-- : totalProductos.textContent = 0;
-                    localStorage.setItem("productos", JSON.stringify(productos));
-                    eliminar.parentElement.parentElement.remove();
-                    Toastify({
-                        text: "El producto fue eliminado del carrito",
-                        duration: 2000,
-                        position: "center",
-                        newWindow: true,
-                        gravity: "top",
-                        stopOnFocus: true,
-                        style: {
-                            background: "#000000",
-                        }
-                    }).showToast();
-                }
-            })    
-        }
-    })
 }
 
 //función para hacer visible el carrito
@@ -120,40 +93,36 @@ carrito.onfocus = () => {
     carro.setAttribute("class", "carro_visible");
 }
 
-//función que escucha los clicks en el carrito de compras
+//escucha los clicks en el carrito de compras
 carro.addEventListener('click', e => {
     realizarCompra(e);
     cerrarCarro(e);
 })
 
-//función que simula la compra y emite una alerta
+//función que simula la compra y emite una alerta (se valida que el carrito no esté vacío)
 const realizarCompra = (e) => {
-    if (e.target.classList.contains('btn_compra')) {
-        const productos = JSON.parse(localStorage.getItem("productos"));
-        if (productos.map(producto => producto.cantidad > 0).includes(true)) {
-            swal({
-                title: "Compra realizada",
-                text: "¡Su compra ha sido exitosa!",
-                icon: "success",
-                button: "Bien!",
-            });
-        }	
-    }
+    productos.forEach(producto => {
+        JSON.parse(localStorage.getItem(`compra${producto.id}`))
+        if (producto.cantidad > 0) {
+            if (e.target.classList.contains('btn_compra')) {
+                swal({
+                    title: "Compra realizada",
+                    text: "¡Su compra ha sido exitosa!",
+                    icon: "success",
+                    button: "¡Bien!",
+                });
+            }	
+        }
+    })
 }
 
-//función para desaparecer el carrito
+//desaparece el carrito
 const cerrarCarro = (e) => {
     if (e.target.classList.contains('btn_cerrar')) {
         carro.setAttribute("class", "carro_invisible");
     }
 }
 
-const total = () => {
-    const productos = JSON.parse(localStorage.getItem("productos"));
-    const total = productos.map(producto => producto.precio * producto.cantidad).reduce((a, b) => a + b, 0);
-    document.getElementById('totalPrecio').innerHTML = `<span>Total: $${total}</span>`;
-}
-total();
 
 
 
